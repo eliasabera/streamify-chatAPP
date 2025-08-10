7005cf4a9dc0d0ea47b1c8ad80413cf57be2eb0d
eliasabera <154503195+eliasabera@users.noreply.github.com>
2025-08-10 10:27:47 +0300

    update

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,15 +1,26 @@
+import User from "../models/User.js";
 
 
-export async function getRecommandedUser(params) {
+export async function getRecommendedUser(req,res) {
     try {
         const currentUserId = req.user.id;
         const currentUser = req.user;
+   
+        const recommendedUser = await User.find({
+            $and: [
+                { _id: { $ne: currentUserId } },
+                { $id: { $nin: currentUser.friends } },
+                {isOnboarded:true}
+           ]
+        })
 
+        res.status(200).json(recommendedUser)
 
 
     
    } catch (error) {
-    
+        console.log(error)
+        res.status(500).json({message:'something wrong! internal server error'})
    }    
 }
 export async function getMyFriends(params) {
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { getRecommandedUser, getMyFriends } from "../controllers/user.controller.js";
+import { getRecommendedUser, getMyFriends } from "../controllers/user.controller.js";
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 
 const router = express.Router();
 router.use(protectRoute)
 
-router.get('/',getRecommandedUser)
+router.get('/',getRecommendedUser)
 router.get('friends',getMyFriends)
 
 
